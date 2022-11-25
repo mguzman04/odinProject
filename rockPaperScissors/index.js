8091bb71f937a5467e4f592cf6f7cb91422f9f56
Michael Guzman <mgzmn4@gmail.com>
2022-11-24 23:23:07 -0600

    Made a round of the game

--- rockPaperScissors/index.js
+++ rockPaperScissors/index.js
@@ -1,7 +1,25 @@
-function getComputerChoice() {
-    // todo
+const choices = ["rock", "paper", "scissors"];
+
+function computerChoice() {
+    return Math.floor(Math.random() * 3);
 };
 
-function playRound(player, computer) {
-    // todo
-}
+function playRound(player, computerChoice) {
+    let playerChoice = choices.indexOf(player.toLowerCase());
+    // tie
+    if (playerChoice === computerChoice){
+        return "It's a tie";
+    }
+    // player wins
+    else if ((playerChoice === 0 && computerChoice === 2) || 
+                (playerChoice === 1 && computerChoice === 0) ||
+                (playerChoice === 2 && computerChoice === 1)) {
+        return `You win! ${choices[playerChoice]} beats ${choices[computerChoice]}`;
+    }
+    else {
+        return `You lose! ${choices[computerChoice]} beats ${choices[playerChoice]}`;
+    }
+};
+
+const playerChoice = prompt("Rock, Paper, or Scissors");
+console.log(playRound(playerChoice, computerChoice()));
