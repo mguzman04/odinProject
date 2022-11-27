a5099cfd7f7eb8e90c644abb3eff6d9ee4dd9534
Michael Guzman <mgzmn4@gmail.com>
2022-11-26 21:45:07 -0600

    Made it a 5 round game

--- rockPaperScissors/index.js
+++ rockPaperScissors/index.js
@@ -4,7 +4,7 @@ function computerChoice() {
     return Math.floor(Math.random() * 3);
 };
 
-function playRound(player, computerChoice) {
+function playRound(player, computerChoice, score) {
     let playerChoice = choices.indexOf(player.toLowerCase());
     // tie
     if (playerChoice === computerChoice){
@@ -14,12 +14,23 @@ function playRound(player, computerChoice) {
     else if ((playerChoice === 0 && computerChoice === 2) || 
                 (playerChoice === 1 && computerChoice === 0) ||
                 (playerChoice === 2 && computerChoice === 1)) {
+        score.player++;
         return `You win! ${choices[playerChoice]} beats ${choices[computerChoice]}`;
     }
     else {
+        score.computer++;
         return `You lose! ${choices[computerChoice]} beats ${choices[playerChoice]}`;
     }
 };
 
-const playerChoice = prompt("Rock, Paper, or Scissors");
-console.log(playRound(playerChoice, computerChoice()));
+function game() {
+    console.log("Rock, Paper, Scissors!");
+    let score = { player: 0, computer: 0};
+    for(let i = 0; i < 5; i++){
+        const playerChoice = prompt("Rock, Paper, or Scissors");
+        console.log(playRound(playerChoice, computerChoice(), score));
+        console.log(`Computer ${score.computer}, You: ${score.player}`);
+    }
+} 
+
+game();
